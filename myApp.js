9834023e5e00fec42814aabde91ffb4987cd3f2f
Stephen Latham <ste.latham@gmail.com>
2023-05-18 11:26:51 +0100

    deletebyid

--- myApp.js
+++ myApp.js
@@ -122,7 +122,14 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+	Person.findByIdAndRemove(personId, function(err, data) {
+
+	if(err) return console.log(err)
+
+	done(null, data)
+
+})
+
 };
 
 const removeManyPeople = (done) => {
