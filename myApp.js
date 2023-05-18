dc7b8a0d84112462e296259f289e5a23ae7eb317
Stephen Latham <ste.latham@gmail.com>
2023-05-18 11:39:01 +0100

    delete many

--- myApp.js
+++ myApp.js
@@ -135,7 +135,13 @@ const removeById = (personId, done) => {
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+	Person.remove({name: nameToRemove}, function(err, data) {
+
+	if(err) return console.log(err)
+
+	done(null, data)
+})
+
 };
 
 const queryChain = (done) => {
